87bbe4a71af631893ea8ea15b73841a11f96897b
Arsh Abel Kumar <abelarshk@gmail.com>
2026-01-23 12:49:06 +0530

    Experiment: Lasso Regression (alpha=0.01)

--- scripts/train.py
+++ scripts/train.py
@@ -19,7 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = Lasso(alpha=0.1)
+model = Lasso(alpha=0.01)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
