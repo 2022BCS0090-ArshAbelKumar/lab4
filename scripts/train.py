07aaa3c10273a2d9b7d93489b6279a9b337d00d2
Arsh Abel Kumar <abelarshk@gmail.com>
2026-01-23 13:23:02 +0530

    Experiment: ElasticNet (alpha=0.1, l1_ratio=0.5)

--- scripts/train.py
+++ scripts/train.py
@@ -4,7 +4,7 @@ import os
 import joblib
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_squared_error, r2_score
 
 os.makedirs("outputs/model", exist_ok=True)
@@ -19,7 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = Lasso(alpha=0.01)
+model =  ElasticNet(alpha=0.1, l1_ratio=0.5)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
