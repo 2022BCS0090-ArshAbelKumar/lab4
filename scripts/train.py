2344bd1e19865f0a329a3a74700eb25c4ca6b07c
Arsh Abel Kumar <abelarshk@gmail.com>
2026-01-23 12:46:11 +0530

    Experiment: Lasso Regression (alpha=0.1)

--- scripts/train.py
+++ scripts/train.py
@@ -4,7 +4,7 @@ import os
 import joblib
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_squared_error, r2_score
 
 os.makedirs("outputs/model", exist_ok=True)
@@ -19,7 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = Ridge(alpha=1.0)
+model = Lasso(alpha=0.1)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
